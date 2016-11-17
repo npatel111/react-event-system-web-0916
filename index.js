e274e26ab2fba718041ae744a4ab1a50d2e8c354
Niti Nrw <niti.patel@flatironschool.com>
2016-11-17 15:37:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much.";
+class Tickler extends React.Component {
+  constructor() {
+    super();
+
+    this.tickle = this.tickle.bind(this);
+  }
+
+  tickle() {
+    console.log('Tee hee!');
+  }
+
+  render() {
+    return (
+      <button>Tickle me!</button>
+    );
+  }
+}
